1b5e97fc8d93ff892073ae4b5fdd3be5e100ff37
unknown <unknown@example.com>
2019-05-22 14:39:07 +0800

    合并代码

---
+++ pages/friends/friends.js
@@ -0,0 +1,25 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+  		imgUrls: [
+	      '/images/friend1.png',
+	      '/images/friend2.png',
+	      '/images/friend3.png',
+	      '/images/friend4.png',
+	      '/images/friend5.png',
+	      '/images/friend6.png',
+	      '/images/friend7.png',
+	      '/images/friend8.png',
+	      '/images/friend9.png',
+	      '/images/friend10.png',
+	      '/images/friend11.png',
+	      '/images/friend12.png'
+	    ]
+  },
+  onLoad: function () {
+    
+  }
+})
---
+++ pages/p_sy/p_sy.js
@@ -0,0 +1,16 @@
+//index.js
+//获取应用实例
+const apps = getApp()
+
+Page({
+  data: {
+   
+  },
+  //事件处理函数
+  bindViewTap: function() {
+   
+  },
+  onLoad: function () {
+    
+  }
+})
