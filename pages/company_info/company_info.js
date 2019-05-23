fb4258afde6c81fe21ccb40558d9786c72f63149
unknown <unknown@example.com>
2019-05-23 08:57:12 +0800

    添加注册路径

--- pages/company_info/company_info.js
+++ pages/company_info/company_info.js
@@ -1,16 +1,28 @@
-//index.js
-//获取应用实例
-const apps = getApp()
 
-Page({
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
   data: {
-   
-  },
-  //事件处理函数
-  bindViewTap: function() {
-   
-  },
-  onLoad: function () {
     
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    loginPage: function (e) {
+      wx.navigateTo({
+        url: '../login/login'
+      })
+    },
+  
   }
 })
